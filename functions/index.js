ad39a2251747f7fa8199f1975e07c21937ce4a78
Hadza <eddieisaac@gmail.com>
2020-02-12 07:08:08 -0700

    Token now gets stored on DB

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,46 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const firebase = require('firebase');
+const pw = require('../src/pw');
+firebase.initializeApp({
+    apiKey: "AIzaSyCcpMOFhkCmdyjy53536DgvlosRrdaC8cM",
+    authDomain: "bitcointracker-kkk.firebaseapp.com",
+    databaseURL: "https://bitcointracker-kkk.firebaseio.com",
+    projectId: "bitcointracker-kkk",
+    storageBucket: "bitcointracker-kkk.appspot.com",
+    messagingSenderId: "2743611449",
+    appId: "1:2743611449:web:59bd2a45b2bd318d719de8",
+    measurementId: "G-513N26R67B"
+});
 admin.initializeApp();
 
-exports.sendPriceAlert = async () => {
-    let deviceTokens = [];
-    const db = admin.firestore.Firestore.collection('users');
+console.log(pw)
 
-    db.get()
-        .then(querySnapshot =>{
-            querySnapshot.forEach((doc) => {
-                deviceTokens.push(doc.data().token)
-            });
-            return undefined
+
+exports.sendPriceAlert = functions.https.onRequest((req, res)  => {
+
+    let deviceTokens = [];
+    //firebase.database().ref(pw.key+'/').on()
+    firebase.database().ref(pw.key+'/').once('value').then(snapshot => {
+        console.log(snapshot, 'snapshot');
+        snapshot.forEach(doc => {
+            deviceTokens.push(doc.val().token)
         })
-        .catch(err => {
-            console.log('No funciono esta madre', err)
-        });
+        console.log(deviceTokens);
+    });
+
+
+
+
     const payload = {
         notification: {
             title: 'Bitcoin tracker alert!',
             body: 'Bitcoin price is down by 5% in 2 hours'
         }
     };
-
-    return await admin.messaging().sendToDevice(deviceTokens, payload);
-};
+    console.log(deviceTokens, 'deviceTokens')
+    return admin.messaging().sendToDevice(deviceTokens, payload);
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
