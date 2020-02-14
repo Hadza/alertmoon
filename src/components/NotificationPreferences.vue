7cd45c542200780ebd5bcaad6014fb8c6e0dbbd0
isaac.osuna <eddieisaac@gmail.com>
2020-02-14 14:02:08 -0700

    Added all rules

--- src/components/NotificationPreferences.vue
+++ src/components/NotificationPreferences.vue
@@ -4,13 +4,110 @@
       <div key="1" v-if="permissionGranted">
         <p :style="{fontSize: 1.8+'em', fontWeight:350, marginBlockStart: 0.1+'em', marginBlockEnd:0.1+'em'}">Preferences</p>
         <hr :style="{marginBlockStart:0.1+'em', marginBlockEnd:0.1+'em'}">
-        <p>Price up <b>5%</b></p>
-        <p :style="{marginLeft:1+'em'}">
-          <p-check class="p-icon p-smooth" color="success-o">
-            <i slot="extra" class="icon mdi mdi-check"></i>
-            <span>In a 30 minutes window</span>
-          </p-check>
-        </p>
+        <div class="container">
+          <div class="item">
+            <p>Price up <b>5%</b> in a</p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span :style="{fontWeight:380}">Select all options</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>10 minutes window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>30 minutes window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>1 hour window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>3 hour window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>6 hour window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>12 hour window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>24 hour window</span>
+              </p-check>
+            </p>
+          </div>
+          <div class="item">
+          <p>Price up <b>10%</b> in a</p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span :style="{fontWeight:380}">Select all options</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>10 minutes window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>30 minutes window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>1 hour window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>3 hour window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>6 hour window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>12 hour window</span>
+              </p-check>
+            </p>
+            <p>
+              <p-check class="p-icon p-smooth" color="success-o">
+                <i slot="extra" class="icon mdi mdi-check"></i>
+                <span>24 hour window</span>
+              </p-check>
+            </p>
+          </div>
+        </div>
       </div>
       <div key="2" v-else >
         <p href="#" class="link-1" @click="askForPermission" :style="{marginBlockStart: 0.1+'em', marginBlockEnd:0.1+'em'}">
@@ -89,4 +186,11 @@ export default {
 label span{
   font-weight:300;
 }
+  .container {
+    display:flex;
+    justify-content: space-evenly;
+  }
+  .container .item {
+    margin: 0 auto 0 0;
+  }
 </style>
